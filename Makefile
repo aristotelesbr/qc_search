e9a23572ce939e531e9724589bc455370d256148
Aristoteles <aristotelesbr2014@gmail.com>
2020-09-06 21:47:22 -0300

    feat(questions): add flow to search by period and Makefile scripts

---
+++ Makefile
@@ -0,0 +1,20 @@
+create:
+	@docker-compose run --rm app bundle exec rake db:create
+
+prepare:
+	@docker-compose run --rm app bundle exec rake db:create db:migrate
+
+migrate:
+	@docker-compose run --rm app bundle exec rake db:migrate
+
+c:
+	@docker-compose run --rm app bundle exec rake app:console
+
+setup:
+	@docker-compose run --rm app bundle exec rake db:drop db:create db:migrate app:setup
+
+teste_prepare:
+	@docker-compose run --rm app bundle exec rake db:test:prepare
+
+test:
+	@docker-compose run --rm app bundle exec rspec $(ARGS)
--- app/use_cases/questions/searcheable/per_period/flow.rb
+++ app/use_cases/questions/searcheable/per_period/flow.rb
@@ -4,8 +4,28 @@ module Questions
   module Searcheable
     module PerPeriod
       class Flow < Micro::Case
+        attributes :init_date, :end_date
+
         def call!
-          Success result: { result: 'PerPeriod' }
+          Success result: { result: query(init_date, end_date) }
+        end
+
+        private
+
+        def query(srt_init_date, srt_end_date)
+          questions_ids = QuestionsAccess.search(
+            load: false,
+            where: { date: srt_init_date.to_date..srt_end_date.to_date },
+            order: { times_accessed: :desc },
+            page: 1,
+            per_page: 100
+          ).results.pluck('question_id')
+
+          perform(questions_ids)
+        end
+
+        def perform(ids)
+          Question.where(id: ids).order(daily_access: :desc)
         end
       end
     end
