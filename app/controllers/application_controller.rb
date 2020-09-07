b1b3d611a312d5e8dfb2905434a9e326105570f4
Aristoteles <aristotelesbr2014@gmail.com>
2020-09-06 23:18:23 -0300

    feat: add call question searchable from to path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@
 # Base controller
 class ApplicationController < Sinatra::Base
   get '/' do
-    { hello: 'world' }.to_json
+    request = if params[:init_date].present? && params[:end_date].present?
+                {
+                  init_date: params[:init_date],
+                  end_date: params[:end_date],
+                  page: params[:page]
+                }
+              else
+                { init_date: params[:init_date], page: params[:page] }
+              end
+
+    questions = Questions::Searcheable::Flow.call(request)
+    { data: questions.data[:result] }.to_json
   end
 end
