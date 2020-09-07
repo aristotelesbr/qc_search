52cfa5ba2b7e239d44ff0f2f3f1ae64ce5499c27
Aristoteles <aristotelesbr2014@gmail.com>
2020-09-06 21:46:49 -0300

    feat(questions): add step to seach last day

--- app/use_cases/questions/searcheable/last_day/steps/search.rb
+++ app/use_cases/questions/searcheable/last_day/steps/search.rb
@@ -5,10 +5,34 @@ module Questions
     module LastDay
       module Steps
         class Search < Micro::Case
-          attributes :date, :cached
+          attributes :date, :cached_data
 
           def call!
-            Success result: { result: 'LastDay' }
+            return Success result: { result: cached_data } if cached?
+
+            Success result: { result: query(date) }
+          end
+
+          private
+
+          def query(srt_date)
+            questions_ids = QuestionsAccess.search(
+              load: false,
+              where: { date: srt_date.to_date },
+              order: { times_accessed: :desc },
+              page: 1,
+              per_page: 100
+            ).results.pluck('question_id')
+
+            perform(questions_ids)
+          end
+
+          def perform(ids)
+            Question.where(id: ids).order(daily_access: :desc)
+          end
+
+          def cached?
+            cached_data.present?
           end
         end
       end
