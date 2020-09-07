5d6da19321a9a6108abe984da54069592b6230c6
Aristoteles <aristotelesbr2014@gmail.com>
2020-09-06 21:44:49 -0300

    chore(test): add enviroment to database from tests

--- config/initializers/active_record_layer.rb
+++ config/initializers/active_record_layer.rb
@@ -14,3 +14,16 @@ configure :production, :development do
     encoding: 'utf8'
   )
 end
+
+configure :test do
+  db = URI.parse(ENV['DATABASE_URL'] || "postgres://#{ENV['POSTGRES_USER']}:#{ENV['POSTGRES_PASSWORD']}@#{ENV['POSTGRES_DB']}/qc_search_test")
+
+  ActiveRecord::Base.establish_connection(
+    adapter: db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    host: db.host,
+    username: db.user,
+    password: db.password,
+    database: db.path[1..-1],
+    encoding: 'utf8'
+  )
+end
